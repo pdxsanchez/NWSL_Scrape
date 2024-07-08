4716818d8bede7adb293258e097fbfe30e714751
Sam Sanchez <pdxsanchez+git@gmail.com>
2024-07-08 00:52:31 -0700

    convert list to a tibble

--- nwsl_scraper.R
+++ nwsl_scraper.R
@@ -15,3 +15,4 @@ u <- "https://fbref.com/en/comps/182/schedule/NWSL-Scores-and-Fixtures"
 ## Read in the webpage, pop it into a table
 xData <- getURL(u)
 table = readHTMLTable(xData, stringsAsFactors=F)
+t <- as_tibble(table)
