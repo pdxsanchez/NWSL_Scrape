8cdea4747687e51e6648162728c06563ea411ff6
Sam Sanchez <pdxsanchez+git@gmail.com>
2024-07-07 17:08:02 -0700

    Added NWSL scores URL and code to scrape it

--- nwsl_scraper.R
+++ nwsl_scraper.R
@@ -8,3 +8,10 @@ library(lubridate)
 library(stringr)
 library(XML) #webscraping
 library(RCurl) #webscraping
+
+## NWSL FBREF Scores web page
+u <- "https://fbref.com/en/comps/182/schedule/NWSL-Scores-and-Fixtures"
+
+## Read in the webpage, pop it into a table
+xData <- getURL(u)
+table = readHTMLTable(xData, stringsAsFactors=F)
