1feae941f65b21248d2fc7e9cbcfd9867400c086
Sam Sanchez <pdxsanchez+git@gmail.com>
2024-07-08 07:53:37 -0700

    Select first df from the list of data frames. assume the first list is the most comprehensive. Add 2023 season

--- nwsl_scraper.R
+++ nwsl_scraper.R
@@ -10,9 +10,16 @@ library(XML) #webscraping
 library(RCurl) #webscraping
 
 ## NWSL FBREF Scores web page
-u <- "https://fbref.com/en/comps/182/schedule/NWSL-Scores-and-Fixtures"
+u24 <- "https://fbref.com/en/comps/182/schedule/NWSL-Scores-and-Fixtures"
+u23 <-"https://fbref.com/en/comps/182/2023/schedule/2023-NWSL-Scores-and-Fixtures"
 
 ## Read in the webpage, pop it into a table
-xData <- getURL(u)
-table = readHTMLTable(xData, stringsAsFactors=F)
-t <- as_tibble(table)
+xData24 <- getURL(u24)
+table24 = readHTMLTable(xData24, stringsAsFactors=F)
+t24a <- table24[[1]]
+t24 <- as_tibble(table24,.name_repair = "unique")
+
+xData23 <- getURL(u23)
+table23 = readHTMLTable(xData23, stringsAsFactors=F)
+t23a <- table23[[1]]
+t23 <- as_tibble(t23a,.name_repair = "unique")
