817222e3a3e5d8af04cb4c37fd171f763a5990ed
Sam Sanchez <pdxsanchez+git@gmail.com>
2024-07-08 22:38:12 -0700

    Clarify comments

--- nwsl_scraper.R
+++ nwsl_scraper.R
@@ -2,35 +2,39 @@ library(usethis)
 library(janitor)
 ####
 library(tidyverse) #filter(); select(); 
-library(readxl) #read_excel()
+library(readxl)    #read_excel()
 library(ggplot2)
 library(lubridate) 
 library(stringr)
-library(XML) #webscraping
+library(XML)   #webscraping
 library(RCurl) #webscraping
 
 ## NWSL FBREF Scores web page
-u24 <- "https://fbref.com/en/comps/182/schedule/NWSL-Scores-and-Fixtures"
+u24 <-"https://fbref.com/en/comps/182/schedule/NWSL-Scores-and-Fixtures"
 u23 <-"https://fbref.com/en/comps/182/2023/schedule/2023-NWSL-Scores-and-Fixtures"
 
-## Read in webpage and format into tibbles.
+## Read in NWSL Sched and fixtures webpages and format into tibbles.
 ## GetURL/ReadHTML performed on 2023 data produces a list of 3 dataframes: 
 ## (1) "sched_all" which we assume is all NWSL weeks including playoffs
 ## (2) "sched_2023_182_1" which we assume is non-playoff weeks
-## (3) "sched_2023_182_2" which we assume is playoff weeks
+## (3) "sched_2023_182_2" which we assume is only playoff weeks
 
 ## GetURL/ReadHTML performed on 2024 data produces a list of 1 dataframes: 
 ## (1) "sched_2024_182_1" which we assume is the schedule to date
 ##
 ## We assume 2024 data will change at some point to resemble 2023 data structure?
 ## We do not, at this date (7/2024), understand what the "182" stands for...
+##
+## We currently (7/24) choose to read in data frame (1) in each list of dataframes 
+## produced by GetURL/ReadHTML. This may change as FBREF modifies its data 
+## structures.
 
-xData24 <- getURL(u24)
+xData24 <- getURL(u24) #2024 NWSL schedule and scores data
 table24 = readHTMLTable(xData24, stringsAsFactors=F)
-t24a <- table24[[1]]
+t24a <- table24[[1]]   #Read in the first dataframe in the list of dataframes
 t24 <- as_tibble(t24a,.name_repair = "unique")
 
-xData23 <- getURL(u23)
+xData23 <- getURL(u23) #2023 NWSL schedule and scores data
 table23 = readHTMLTable(xData23, stringsAsFactors=F)
-t23a <- table23[[1]]
+t23a <- table23[[1]]  #Read in the first dataframe in the list of dataframes
 t23 <- as_tibble(t23a,.name_repair = "unique")
